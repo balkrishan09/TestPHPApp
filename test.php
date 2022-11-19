b81d89e82a6f2b7683efcaa6377031508adff32d
Balkrishan Vyas <balkrishan.vyas@adglobal360.com>
2022-11-19 13:08:43 +0530

    Add changes

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
 echo "This is an example of AWS CI CD Pileline using Github/AWS Code Pipeline/AWS ElasticBeanstalk";
-//echo "<br>";
-//echo "We can see changes reflecting real time!! Great!";
+echo "<br>";
+echo "We can see changes reflecting real time!! Great!";
 ?>
